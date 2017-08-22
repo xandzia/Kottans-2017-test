7dfebd17c373af60367fe0f30dee188acd483ac2
xandzia <stepashko.ann@gmail.com>
2017-08-22 21:53:18 +0300

    .....jquery - не хотел добавлять класс в форме. Done

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 $(".btn").click(function() { 
     $(".search").addClass('activeHeadForm');
     $(".container>.search>label").addClass('activeLogo');     
+    $(".search-input").css('position', 'static');     
 });
