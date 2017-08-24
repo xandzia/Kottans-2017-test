5e000c57ee121cee93ba7cdcdefae59460936b18
xandzia <stepashko.ann@gmail.com>
2017-08-24 16:30:36 +0300

    add filters, sorting, dialog, card. только внешний вид

--- scripts.js
+++ scripts.js
@@ -1,5 +1,65 @@
 $(".btn").click(function() { 
     $(".search").addClass('activeHeadForm');
-    $(".container>.search>label").addClass('activeLogo');     
-    $(".search-input").css('position', 'static');     
+    $(".search>label").addClass('activeLogo');     
+    $(".search-input").css('position', 'relative');     
+    $(".filterssorting").css('display', 'flex');     
+});
+//выпадающее меню фильтров и сортировки
+$(".btn-filter").click(function() {
+    $(".filters-menu").css('display', 'block');
+    $("button").attr("aria-expanded","true");
+});
+$(".menu-close").click(function() {
+    $(".filters-menu").css('display', 'none');
+    $("button").attr("aria-expanded","false");
+});
+//закрыть меню при активности вне его
+$(document).mouseup(function (e) {
+    var container = $(".filters-menu");
+    if (container.has(e.target).length === 0){
+        container.hide();
+    }
+});
+
+$(".btn-sorting").click(function() {
+    $(".sorting-menu").css('display', 'block');
+    $("button").attr("aria-expanded","true");
+});
+$(".menu-close").click(function() {
+    $(".sorting-menu").css('display', 'none');
+    $("button").attr("aria-expanded","false");
+});
+//закрыть меню при активности вне его
+$(document).mouseup(function (e) {
+    var container = $(".sorting-menu");
+    if (container.has(e.target).length === 0){
+        container.hide();
+    }
+});
+//function close(){
+//    var btn1 = $(".btn-filter");
+//    var btn2 = $(".btn-sorting");
+//    var menu1 = $(".filters-menu");
+//    var menu1 = $(".sorting-menu");
+//    if 
+//        btn1.click(function() {
+//    menu1.css('display', 'block');
+//    $("button").attr("aria-expanded","true");
+//});
+//    else btn1=btn2;
+//    menu1 = menu2;
+//    
+//};
+
+// register modal component
+Vue.component('modal', {
+  template: '#modal-template'
+})
+
+// start app
+new Vue({
+  el: '#app',
+  data: {
+    showModal: false
+  }
 });
